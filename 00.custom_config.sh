49bcdb479519909e161ea313ec8423006018307f
nozomi_nishiohara <nozomi.nishinohara@belldata.co.jp>
2019-10-28 16:47:26 +0900

    Dockerfile編集

--- 00.custom_config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i "s/^.*max_prepared_transactions\s*=\s*\(.*\)$/max_prepared_transactions = 1000/" "$PGDATA"/postgresql.conf
-sed -i "s/^.*max_connections\s*=\s*\(.*\)$/max_connections = 1000/" "$PGDATA"/postgresql.conf
